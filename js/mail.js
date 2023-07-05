046642d2220b10fd05af9901201d51e55cf64a29
Pietro-Leone <leonepietro1999@gmail.com>
2023-07-05 15:31:23 +0200

    fine primo esercizio

---
+++ js/mail.js
@@ -0,0 +1,26 @@
+// dichiarazione variabili
+const body = document.querySelector("body")
+const inputEmail = document.getElementById("InputEmail");
+const btnLogin = document.getElementById("btn-login");
+// array email consentite
+const allowedEmail = ["boolean@gmail.com", "class102@gmail.com", "loremipsum@gmail.com", "user@gmail.com"]
+
+// Evento al click del button Login
+btnLogin.addEventListener("click", function () {
+    const email = inputEmail.value;
+
+    for (let i = 0; i < allowedEmail.length; i++) {
+
+        if (email === allowedEmail[i]) {
+            console.log("La mail inserita è corretta!");
+            body.style.backgroundColor = "#198754";
+            alert("La mail inserita è corretta!")
+        }
+        else if (i === allowedEmail.length - 1) {
+            console.log("la mail inserita è sbagliata");
+            body.style.backgroundColor = "#dc3545";
+            alert("la mail inserita è sbagliata")
+        }
+
+    }
+})
